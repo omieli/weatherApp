712fb6cec3e13f6dbb1d6c16b137452d162786f8
omieli <mieli.ohad@gmail.com>
2020-09-13 16:11:32 +0300

    Backend part

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const mongoose = require('mongoose')
+
+
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+app.use(express.static(path.join(__dirname, '..', 'dist')))
+app.use(express.static(path.join(__dirname,'..','node_modules')))
+
+const api = require('./routes/api')
+app.use('/', api)
+
+mongoose.connect('mongodb://localhost/whetherAppDB', { useNewUrlParser: true })
+
+
+
+const port = 3000
+app.listen(port, function () {
+    console.log(`Running on port ${port}`)
+})
