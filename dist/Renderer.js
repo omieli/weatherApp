f5d8b26d89e2a0a7f9f67135828612e3563c2eb8
omieli <mieli.ohad@gmail.com>
2020-09-13 19:21:49 +0300

    render part

---
+++ dist/Renderer.js
@@ -0,0 +1,10 @@
+class Render {
+    renderData(allCityData) { 
+        const source = $('#city-template').html()
+        const template = Handlebars.compile(source)
+        const newHTML = template(allCityData)
+        $('#container').empty().append(newHTML)
+    }
+
+    
+}
