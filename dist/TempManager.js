56d45a732d13a01db66eccec2a1f06f8e0e19802
omieli <mieli.ohad@gmail.com>
2020-09-16 10:47:46 +0300

    finish - without extensions

--- dist/TempManager.js
+++ dist/TempManager.js
@@ -1,14 +1,6 @@
 /**MVC - CONTROLLER*/
 
-// class City {
-//     constructor(name, temperature, conditions, icon, flag) {
-//         this.name = name,
-//         this.temperature = temperature,
-//         this.conditions = conditions,
-//         this.icon = icon    
-//         this.flag = flag
-//      }
-// }
+
 
 class TempManager {
     constructor() { 
@@ -23,9 +15,8 @@ class TempManager {
     getCityData = async function (cityName) {
         let info = await $.get(`city/${cityName}`)
         info = JSON.parse(info)
-        let iconUrl = "http://openweathermap.org/img/wn/" + info.weather[0].icon + "@2x.png"
+        let iconUrl = "http://openweathermap.org/img/wn/" + info.weather[0].icon + ".png"
         const selectedCity = { name: info.name,temperature: Math.round(info.main.temp - 273.15),conditions: info.weather[0].description,conditionPic: iconUrl, flag: true}
-        // const selectedCity = new City (info.name, Math.round(info.main.temp - 273.15) ,info.weather[0].description,iconUrl, true)
 
         this.cityData.push(selectedCity)
     }
--- dist/main.js
+++ dist/main.js
@@ -3,20 +3,20 @@ tempManager = new TempManager
 
 render.loadPage()
 
-$('button').on('click', function () {
+$('#button').on('click', function () {
     let cityInput = $('input').val()
     render.handleSearch(cityInput)
     render.loadPage()
 })
 
-$('#container').on('click','.addIcon', function () { 
-    let cityName= $(this).siblings('.name').text()
+$('#container').on('click','#addIcon', function () { 
+    let cityName= $(this).siblings('.title').text()
     tempManager.saveCity(cityName)
     render.loadPage()
 })
 
-$('#container').on('click','.removeIcon', function () { 
-    let cityName= $(this).siblings('.name').text()
+$('#container').on('click','#removeIcon', function () { 
+    let cityName= $(this).siblings('.title').text()
     tempManager.removeCity(cityName)
     render.loadPage()
 })
